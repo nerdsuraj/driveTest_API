c4c844b868d4fc27ffe67fe10fa23f0be4149556
VPsuraj <suraj@valuepitch.com>
2024-05-13 21:41:00 +0530

    trying to remove the cors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.momngoURI, { useNewUrlParser: true });
 
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000','https://driveacess.netlify.app'],
     methods: ['GET', 'POST'],
     credentials: true
 }));
